864254e0343bed7b495957443a71a167eaf155a8
pong02 <zhongzhe.pong@qinetics.local>
2022-12-29 14:19:45 +0800

    More functions

--- real.py
+++ real.py
@@ -1,4 +1,5 @@
-# This program prints Hello, world but 5 times
+# This program prints Hello, world but 5 times and with a name input
 
+name = str(input("Please enter your name: "))
 for i in range(5):
-  print('Hello, world!')
+  print('Hello",name,"!')
